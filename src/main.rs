89146aa2858aab46554714db6b8f71187906f1d6
Mick Sayson <mick@sayson.com>
2017-04-03 20:46:01 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,80 +2,79 @@ fn idx2(x: usize, y: usize, pitch: usize) -> usize {
     y * pitch + x
 }
 
+struct MemeitDrawer {
+    pitch: usize,
+    input: String,
+    canvas: Vec<char>,
+}
+
+impl MemeitDrawer {
+    pub fn new(input: &str) -> MemeitDrawer {
+        let len = input.len();
+        let mut canvas = Vec::new();
+
+        let width = len * 3;
+        let height = len * 3 / 2;
+        for _ in 0..height {
+            for _ in 0..width {
+                canvas.push(' ');
+            }
+            canvas.push('\n');
+        }
+        canvas.pop();
+
+        MemeitDrawer {
+            pitch: width + 1,
+            input: input.to_string(),
+            canvas: canvas,
+        }
+    }
+
+    pub fn draw(&mut self) {
+        let len = self.input.len();
+        self.draw2d(if len % 2  == 0{len } else { len - 1}, 0);
+        self.draw2d(0, len / 2);
+        self.draw_diagonals();
+    }
+
+    fn draw2d(&mut self, start_x: usize, start_y: usize) {
+        let len =self.input.len();
+        for (i, c) in self.input.chars().enumerate() {
+            // Top row
+            self.canvas[idx2(i * 2 + start_x, start_y, self.pitch)] = c;
+            // Left column
+            self.canvas[idx2(start_x, i + start_y, self.pitch)] = c;
+            // Bottom row
+            self.canvas[idx2((len - 1) * 2 - (i * 2) + start_x, start_y + len - 1, self.pitch)] = c;
+            // Right column
+            self.canvas[idx2((len - 1) * 2 + start_x ,start_y + len - 1 - i, self.pitch)] = c;
+        }
+    }
+
+    fn draw_diagonals(&mut self) {
+        let len = self.input.len();
+        for i in 1..len/2 {
+            self.canvas[idx2((len / 2 - i) * 2, i, self.pitch)] = '/';
+            self.canvas[idx2((len / 2 - i) * 2, i + len - 1, self.pitch)] = '/';
+            self.canvas[idx2((len / 2 - i) * 2 + len * 2 - 2, i, self.pitch)] = '/';
+            self.canvas[idx2((len / 2 - i) * 2 + len * 2 - 2, i + len - 1, self.pitch)] = '/';
+        }
+    }
+}
+
 fn main() {
     let mut input = String::new();
     for s in std::env::args().skip(1) {
         for c in s.chars() {
             input.push(c.to_uppercase().next().expect("No uppercase variant"));
         }
+        input.push(' ');
     }
-    let len = input.len();
+    input.pop();
 
-    let mut output = vec![];
+    let mut memeit_drawer = MemeitDrawer::new(&input);
+    memeit_drawer.draw();
 
-    let width = len * 3;
-    let height = len * 3 / 2;
-    for _ in 0..height {
-        for _ in 0..width {
-            output.push(' ');
-        }
-        output.push('\n');
-    }
-    output.pop();
-    // Compensate for '\n'
-    let width = width + 1;
-
-/*
-    T E S T
-  / E   / S
-T E S T   E
-E   T S E T
-S /   E /
-T E S T
-
-
-        T E S T T E S T
-      / E           / S
-    /   S         /   E
-  /     T       /     T
-T E S T T E S T       T
-E       E     S       S
-S       S     E       E
-T       T S E T T S E T
-T     /       T     /
-E   /         S   /
-S /           E /
-T S E T T S E T
-
-
-*/
-{
-    let mut draw2d = |start_x, start_y| {
-        for (i, c) in input.chars().enumerate() {
-            // Top row
-            output[idx2(i * 2 + start_x, start_y, width)] = c;
-            // Left column
-            output[idx2(start_x, i + start_y, width)] = c;
-            // Bottom row
-            output[idx2((len - 1) * 2 - (i * 2) + start_x, start_y + len - 1, width)] = c;
-            // Right column
-            output[idx2((len - 1) * 2 + start_x ,start_y + len - 1 - i, width)] = c;
-        }
-    };
-
-    draw2d(if len % 2  == 0{len } else { len - 1}, 0);
-    draw2d(0, len / 2);
-}
-
-    for i in 1..len/2 {
-        output[idx2((len / 2 - i) * 2, i, width)] = '/';
-        output[idx2((len / 2 - i) * 2, i + len - 1, width)] = '/';
-        output[idx2((len / 2 - i) * 2 + len * 2 - 2, i, width)] = '/';
-        output[idx2((len / 2 - i) * 2 + len * 2 - 2, i + len - 1, width)] = '/';
-    }
-
-
-
-    println!("{}", output.into_iter().collect::<String>());
+    println!("{}", memeit_drawer.canvas .into_iter().collect::<String>());
 
 }
